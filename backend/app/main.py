e770fa28f857a63f258c6b7f74faa43c0a061935
sravanthi020198-repo <addlasravanthi123@gmail.com>
2025-07-06 14:45:06 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import auth, issues, dashboard
+from fastapi.openapi.utils import get_openapi
 
 app = FastAPI()
 
@@ -19,3 +20,12 @@ app.include_router(dashboard.router, prefix="/api/dashboard")
 @app.get("/")
 def read_root():
     return {"status": "Mini SaaS Tracker Running"}
+
+@app.get("/api/docs", include_in_schema=False)
+def custom_openapi():
+    return get_openapi(
+        title="Issues & Insights Tracker API",
+        version="1.0.0",
+        routes=app.routes
+    )
+
